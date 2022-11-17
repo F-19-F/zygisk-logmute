14ef1d94c5e8a86abf78a68646bb8e1143c04733
ff19 <swutangtf@email.swu.edu.cn>
2022-11-17 11:31:59 +0800

    demo

--- module/jni/main.cpp
+++ module/jni/main.cpp
@@ -1,8 +1,10 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dlfcn.h>
 #include <android/log.h>
-
+#include <sys/mman.h>
+#include <pmparser.h>
 #include "zygisk.hpp"
 
 using zygisk::Api;
@@ -10,8 +12,8 @@ using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "Magisk", __VA_ARGS__)
-
-class MyModule : public zygisk::ModuleBase {
+extern void nopFunArm64(void* addr);
+class LogMute : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
         this->api = api;
@@ -34,6 +36,10 @@ private:
     JNIEnv *env;
 
     void preSpecialize(const char *process) {
+#ifndef  __LP64__
+        LOGD("non 64");
+        return;
+#endif
         // Demonstrate connecting to to companion process
         // We ask the companion for a random number
         unsigned r = 0;
@@ -41,6 +47,42 @@ private:
         read(fd, &r, sizeof(r));
         close(fd);
         LOGD("example: process=[%s], r=[%u]\n", process, r);
+        void* handle = dlopen("liblog.so",RTLD_NOW);
+
+        if(handle == NULL){
+            LOGD("handle = NULL");
+            return;
+        }
+        void* funPtr = dlsym(handle,"__android_log_logd_logger");
+        if(funPtr == NULL){
+            LOGD("funPtr = NULL");
+            return;
+        }
+        procmaps_struct* maps_tmp=NULL;
+        procmaps_iterator* maps = pmparser_parse(-1);
+        if(maps==NULL){
+            LOGD("[map]: cannot parse the memory map");
+            return;
+        }
+        while( (maps_tmp = pmparser_next(maps)) != NULL){
+            if((void *)funPtr >  maps_tmp->addr_start && (void *)funPtr < maps_tmp->addr_end){
+                // pmparser_print(maps_tmp,0);
+                // +w
+                if (mprotect(maps_tmp->addr_start,maps_tmp->length,PROT_EXEC|PROT_WRITE|PROT_READ) != 0){
+                    LOGD("mprotect : %s", strerror(errno));
+                    break;
+                }
+                nopFunArm64(funPtr);
+                // -w
+                if (mprotect(maps_tmp->addr_start,maps_tmp->length,PROT_EXEC|PROT_READ) != 0){
+                    LOGD("mprotect : %s", strerror(errno));
+                    break;
+                }
+                break;
+            }
+        }
+        pmparser_free(maps);
+
 
         // Since we do not hook any functions, we should let Zygisk dlclose ourselves
         api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
@@ -60,5 +102,5 @@ static void companion_handler(int i) {
     write(i, &r, sizeof(r));
 }
 
-REGISTER_ZYGISK_MODULE(MyModule)
+REGISTER_ZYGISK_MODULE(LogMute)
 REGISTER_ZYGISK_COMPANION(companion_handler)
